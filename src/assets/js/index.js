9c9d415dfa887d13774a48cc2dd27d50aa4831bd
ZipName <52377140+ZipName80@users.noreply.github.com>
2024-08-02 18:30:16 +0200

    Added References

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -30,9 +30,9 @@ class Splash {
 
     async startAnimation() {
         let splashes = [
-            { "message": "Je... vie...", "author": "Luuxis" },
-            { "message": "Salut je suis du code.", "author": "Luuxis" },
-            { "message": "Linux n'est pas un os, mais un kernel.", "author": "Luuxis" }
+            { "message": "je m'en tape la b*** sur la table", "author": "NTILan" },
+            { "message": "Viens on ouvre le port du Homerun", "author": "AlexK" },
+            { "message": "C'est un bucheron", "author": "AlexK" }
         ];
         let splash = splashes[Math.floor(Math.random() * splashes.length)];
         this.splashMessage.textContent = splash.message;
